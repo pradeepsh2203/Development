d9ae8eb5439a9a99abeeac83cd419da6dbe6a1b1
zn08 <56965113+SATAKSHI08@users.noreply.github.com>
2020-12-22 16:15:49 +0530

    updated app.js

--- resource/src/App.js
+++ resource/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Vision from './components/Vision';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App" >
+    <div className="page-container">
+      <div className="content-wrap">
          <Header/>
          <div width='100px' height='100px'></div>
          <Vision />
+         <Footer/>
     </div>
   );
 }
