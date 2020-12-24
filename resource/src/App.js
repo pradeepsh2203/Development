e6b663bbf078f46dbdb810253aad6d8dd4cda24e
Pradeep Sharma <pradeep.sharma.cd.mec19@itbhu.ac.in>
2020-12-24 12:39:58 +0530

    Deploying Carousel

--- resource/src/App.js
+++ resource/src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Vision from './components/Vision';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Vision from "./components/Vision";
+import Footer from "./components/Footer";
+import Caoursel from "./components/Caoursel";
 
 function App() {
   return (
     <div className="page-container">
       <div className="content-wrap">
-         <Header/>
-         <div width='100px' height='100px'></div>
-         <Vision />
-    </div>
-         <Footer/>
+        <Header />
+        <Caoursel />
+        <Vision />
+      </div>
+      <Footer />
     </div>
   );
 }
